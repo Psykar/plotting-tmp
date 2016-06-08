3940d8cb99193c1ac539754616e895f6dfca61eb
Louis des Landes <ldeslandes@swin.edu.au>
2016-06-08 11:41:22 +1000

    Initial testing

--- test_plotly.py
+++ test_plotly.py
@@ -1,44 +1,50 @@
 import plotly.offline as py
-import plotly.graph_objs as go
+
+from plotly.tools import FigureFactory as FF
 
 import numpy as np
+from scipy.spatial import Delaunay
 
-# Creating the data
-x = np.linspace(-5, 5, 50)
-y = np.linspace(-5, 5, 50)
-xGrid, yGrid = np.meshgrid(y, x)
-R = np.sqrt(xGrid ** 2 + yGrid ** 2)
-z = np.sin(R)
+class LightCone():
+    def __init__(self, box_size=50, ra_min=0, ra_max=10, dec_min=0, dec_max=10):
+        self.box_size = box_size
+        self.ra_mix = ra_min
+        self.ra_max = ra_max
+        self.dec_min = dec_min
+        self.dec_max = dec_max
 
-# Creating the plot
-lines = []
-line_marker = dict(color='#0066FF', width=2)
-for i, j, k in zip(xGrid, yGrid, z):
-    lines.append(go.Scatter3d(x=i, y=j, z=k, mode='lines', line=line_marker))
+    def plot(self):
 
-layout = go.Layout(
-    title='Wireframe Plot',
-    scene=dict(
-        xaxis=dict(
-            gridcolor='rgb(255, 255, 255)',
-            zerolinecolor='rgb(255, 255, 255)',
-            showbackground=True,
-            backgroundcolor='rgb(230, 230,230)'
-        ),
-        yaxis=dict(
-            gridcolor='rgb(255, 255, 255)',
-            zerolinecolor='rgb(255, 255, 255)',
-            showbackground=True,
-            backgroundcolor='rgb(230, 230,230)'
-        ),
-        zaxis=dict(
-            gridcolor='rgb(255, 255, 255)',
-            zerolinecolor='rgb(255, 255, 255)',
-            showbackground=True,
-            backgroundcolor='rgb(230, 230,230)'
-        )
-    ),
-    showlegend=False,
-)
-fig = go.Figure(data=lines, layout=layout)
-py.offline.plot(fig, filename='elevations-3d-surface')
+
+        u = np.linspace(0, np.pi * 2, 100)
+        v = np.linspace(0, np.pi * 2, 100)
+        u, v = np.meshgrid(u, v)
+        u = u.flatten()
+        v = v.flatten()
+
+        x = u
+        y = u * np.cos(v)
+        z = u * np.sin(v)
+
+        points2D = np.vstack([u, v]).T
+        tri = Delaunay(points2D)
+        simplices = tri.simplices
+
+        # define a function for the color assignment
+        def dist_from_x_axis(x, y, z):
+            return abs(x)
+
+        fig1 = FF.create_trisurf(x=x, y=y, z=z,
+                                 colormap=['rgb(255, 10, 120)', 'rgb(255, 100, 255)', ],
+                                 color_func=dist_from_x_axis,
+                                 simplices=simplices, title="Light Cone",
+                                 showbackground=False, gridcolor='rgb(255, 20, 160)',
+                                 plot_edges=False, aspectratio=dict(x=1, y=1, z=0.75))
+        py.plot(fig1, filename="Light Cone.html")
+
+
+if __name__ == '__main__':
+    cone = LightCone()
+    cone.plot()
+
+
