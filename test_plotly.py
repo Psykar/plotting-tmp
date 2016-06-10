9105d8deab39f6228b8d2a5926fd3397b5375e15
Louis des Landes <ldeslandes@swin.edu.au>
2016-06-10 14:44:18 +1000

    Yay cones!

--- test_plotly.py
+++ test_plotly.py
@@ -1,50 +1,112 @@
 import plotly.offline as py
 
-from plotly.tools import FigureFactory as FF
+import random
 
 import numpy as np
-from scipy.spatial import Delaunay
+import plotly.graph_objs as go
+
 
 class LightCone():
-    def __init__(self, box_size=50, ra_min=0, ra_max=10, dec_min=0, dec_max=10):
+    def __init__(self, box_size=50, ra_min=0, ra_max=10, dec_min=0, dec_max=10, seed=None):
         self.box_size = box_size
         self.ra_mix = ra_min
         self.ra_max = ra_max
         self.dec_min = dec_min
         self.dec_max = dec_max
+        if seed is None:
+            seed = random.randint(0, 1000000)
+        self.seed = seed
 
-    def plot(self):
+    def calc(self):
+        origin = self.calc_origin()
 
 
-        u = np.linspace(0, np.pi * 2, 100)
-        v = np.linspace(0, np.pi * 2, 100)
-        u, v = np.meshgrid(u, v)
-        u = u.flatten()
-        v = v.flatten()
+    def calc_origin(self):
+        self.calc_subcone_angle()
+        self.calc_subcone_origin()
 
-        x = u
-        y = u * np.cos(v)
-        z = u * np.sin(v)
+    def calc_subcone_angle(self):
+        pass
 
-        points2D = np.vstack([u, v]).T
-        tri = Delaunay(points2D)
-        simplices = tri.simplices
+    def calc_subcone_origin(self):
+        pass
 
-        # define a function for the color assignment
-        def dist_from_x_axis(x, y, z):
-            return abs(x)
+    def plot(self, origin, ra, dec):
+        # Convert ra / dec to radians
+        ra = ra * np.pi / 180
+        dec = dec * np.pi / 180
 
-        fig1 = FF.create_trisurf(x=x, y=y, z=z,
-                                 colormap=['rgb(255, 10, 120)', 'rgb(255, 100, 255)', ],
-                                 color_func=dist_from_x_axis,
-                                 simplices=simplices, title="Light Cone",
-                                 showbackground=False, gridcolor='rgb(255, 20, 160)',
-                                 plot_edges=False, aspectratio=dict(x=1, y=1, z=0.75))
-        py.plot(fig1, filename="Light Cone.html")
+        origx, origy, origz = origin
+
+        accuracy = 360
+        h = self.box_size
+        # Calculate cone size from min / max dec, ignore ra?
+        cone_angle = (self.dec_max - self.dec_min) * np.pi / 180
+        r = h * np.sin(cone_angle)
+
+        phi = np.linspace(0, np.pi * 2, accuracy)
+        u = np.linspace(0, h, accuracy)
+        phigrid, ugrid,  = np.meshgrid(phi, u)
+
+        x = h - ugrid
+        y = ((h - ugrid) / h) * r * np.cos(phigrid)
+        z = ((h - ugrid) / h) * r * np.sin(phigrid)
+
+        # Dec has to come first otherwise it's rotation wouldn't
+        # be around an axis and maths gets complicated
+        # Rotate by dec, around y axis
+        xnew = x * np.cos(dec) - z * np.sin(dec)
+        znew = z * np.cos(dec) + x * np.sin(dec)
+        x = xnew
+        z = znew
+
+        # Rotate by RA, around z axis
+        xnew = x * np.cos(ra) - y * np.sin(ra)
+        ynew = y * np.cos(ra) + x * np.sin(ra)
+        x = xnew
+        y = ynew
+
+        x = x + origx
+        y = y + origy
+        z = z + origz
+
+        surface = go.Surface(x=x, y=y, z=z)
+        data = [surface]
+
+        layout = go.Layout(
+            title='Parametric Plot',
+            scene=dict(
+                xaxis=dict(
+                    gridcolor='rgb(255, 255, 255)',
+                    zerolinecolor='rgb(255, 255, 255)',
+                    showbackground=True,
+                    backgroundcolor='rgb(230, 230,230)',
+                    range=[0, self.box_size],
+                ),
+                yaxis=dict(
+                    gridcolor='rgb(255, 255, 255)',
+                    zerolinecolor='rgb(255, 255, 255)',
+                    showbackground=True,
+                    backgroundcolor='rgb(230, 230,230)',
+                    range=[0, self.box_size],
+                ),
+                zaxis=dict(
+                    gridcolor='rgb(255, 255, 255)',
+                    zerolinecolor='rgb(255, 255, 255)',
+                    showbackground=True,
+                    backgroundcolor='rgb(230, 230,230)',
+                    range=[0, self.box_size],
+                ),
+                aspectmode='cube',
+            )
+        )
+
+        fig = go.Figure(data=data, layout=layout)
+        py.plot(fig, filename="Light_Cone.html")
 
 
 if __name__ == '__main__':
     cone = LightCone()
-    cone.plot()
+    cone.plot((0, 0, 0), 45, 45)
 
 
